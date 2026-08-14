d0b357de6bf55215f9d172818e82d79cfe315cb2
Khushi23111 <aaghost420@gmail.com>
2026-08-14 14:37:49 +0600

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://news-portal-backend-sath.onrender.com/api",
 });
 
 export default api;
